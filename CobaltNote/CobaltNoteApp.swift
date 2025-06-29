89e5289499a4a6dd4e6d1e490629fe5e71477d08
“cobalt-s” <stameycobalt@example.com>
2025-06-29 12:34:07 -0700

    Initial Commit

---
+++ CobaltNote/CobaltNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  CobaltNoteApp.swift
+//  CobaltNote
+//
+//  Created by Cobalt Stamey on 6/29/25.
+//
+
+import SwiftUI
+
+@main
+struct CobaltNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CobaltNoteTests/CobaltNoteTests.swift
@@ -0,0 +1,17 @@
+//
+//  CobaltNoteTests.swift
+//  CobaltNoteTests
+//
+//  Created by Cobalt Stamey on 6/29/25.
+//
+
+import Testing
+@testable import CobaltNote
+
+struct CobaltNoteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
